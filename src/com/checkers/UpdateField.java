5e8869a1d1f5e08cdc07cbfd4ecff5a0bd5fe05f
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-09-09 20:28:38 +0300

    update class UpdateField

--- src/com/checkers/UpdateField.java
+++ src/com/checkers/UpdateField.java
@@ -16,10 +16,12 @@ public class UpdateField {
     protected UpdateField(String[][] array, String string) throws Exception {
         this.checker = insertCharacter(string.substring(0, 3));
         this.stepChecker = insertSpaces(string.substring(4));
-        this.currentCheckerIndexI = findIndex(array, this.checker).getIndexI();
-        this.currentCheckerIndexJ = findIndex(array, this.checker).getIndexJ();
-        this.stepCheckerIndexI = findIndex(array, this.stepChecker).getIndexI();
-        this.stepCheckerIndexJ = findIndex(array, this.stepChecker).getIndexJ();
+        FindIndexTuple2 findIndexCurrentChecker = new FindIndexTuple2(array, this.checker);
+        FindIndexTuple2 findIndexCurrentStepChecker = new FindIndexTuple2(array, this.stepChecker);
+        this.currentCheckerIndexI = findIndexCurrentChecker.getIndexI();
+        this.currentCheckerIndexJ = findIndexCurrentChecker.getIndexJ();
+        this.stepCheckerIndexI = findIndexCurrentStepChecker.getIndexI();
+        this.stepCheckerIndexJ = findIndexCurrentStepChecker.getIndexJ();
         this.field = update(array);
     }
 
@@ -40,19 +42,87 @@ public class UpdateField {
         if (equalNumbersHorizontal(this.checker, this.stepChecker)) throw new Exception("Move is not possible(num)");
 
         // char => int => 97...104
-        int characterChecker = checker.charAt(2);
-        int characterStepChecker = stepChecker.charAt(1);
+        int characterChecker = this.checker.charAt(2);
+        int characterStepChecker = this.stepChecker.charAt(1);
 
         if (characterChecker + 1 == characterStepChecker || characterChecker - 1 == characterStepChecker) {
-            array[this.currentCheckerIndexI][this.currentCheckerIndexJ] = String.format(" %s ", this.checker.substring(2));
-            array[this.stepCheckerIndexI][this.stepCheckerIndexJ] = String.format(
-                    "%s%s", this.checker.substring(0,2), this.stepChecker.trim());
-            return array;
-        } else if (characterChecker + 2 == characterStepChecker || characterChecker - 2 == characterStepChecker) {
+            return changeField(array);
+        } else if (characterChecker + 2 == characterStepChecker) {
+
+            if (this.checker.charAt(3) + 1 == this.stepChecker.charAt(2) - 1) {
+                String intermediateCage = insertSpaces(plusCharacter(this.checker.charAt(2))
+                        .concat(plusCharacter(this.checker.charAt(3))));
+                return changeIntermediateCage(array, intermediateCage);
+            }
+
+            String intermediateCage = insertSpaces(plusCharacter(this.checker.charAt(2))
+                    .concat(minusCharacter(this.checker.charAt(3))));
+            return changeIntermediateCage(array, intermediateCage);
+
+        } else if (characterChecker - 2 == characterStepChecker) {
+
+            if (this.checker.charAt(3) + 1 == this.stepChecker.charAt(2) - 1) {
+                String intermediateCage = insertSpaces(plusCharacter(this.checker.charAt(2))
+                        .concat(minusCharacter(this.checker.charAt(3))));
+                return changeIntermediateCage(array, intermediateCage);
+            }
+
+            String intermediateCage = insertSpaces(minusCharacter(this.checker.charAt(2))
+                    .concat(plusCharacter(this.checker.charAt(3))));
+            return changeIntermediateCage(array, intermediateCage);
 
         } else throw new Exception("Not the possibility of a move");
+    }
 
-        return new String[17][17];
+    private String[][] changeIntermediateCage(String[][] array, String intermediateCage) throws Exception {
+        FindIndexTuple2 intermediateCageIndex = new FindIndexTuple2(array, intermediateCage);
+
+        if (intermediateCageIndex.getIndexI() != 0) {
+            throw new Exception("The intermediate cell is free. Move is not possible ");
+        } else {
+            if (findMatch(array, this.checker.charAt(0), intermediateCage)) {
+                throw new Exception("Checkers of the same color are not taken");
+            } else {
+                var field = changeField(array);
+
+                String character = 'W' == this.checker.charAt(0) ? "B" : "W";
+                String middle = String.format("%s-%s", character, intermediateCage.trim());
+                FindIndexTuple2 findIndexTuple = new FindIndexTuple2(array, middle);
+                field[findIndexTuple.getIndexI()][findIndexTuple.getIndexJ()] = intermediateCage;
+
+                return field;
+            }
+        }
+    }
+
+    private String[][] changeField(String [][] array) {
+        array[this.currentCheckerIndexI][this.currentCheckerIndexJ] = String.format(" %s ", this.checker.substring(2));
+        array[this.stepCheckerIndexI][this.stepCheckerIndexJ] = String.format(
+                "%s%s", this.checker.substring(0,2), this.stepChecker.trim());
+        return array;
+    }
+
+    private String plusCharacter(char c) {
+        char character = (char) (c + 1);
+        return String.valueOf(character);
+    }
+
+    private String minusCharacter(char c) {
+        char character = (char) (c - 1);
+        return String.valueOf(character);
+    }
+
+    private boolean findMatch(String[][] array, char charChecker, String intermediateCage) {
+        String character = String.valueOf(charChecker);
+        String str = String.format("%s-%s", character, intermediateCage.trim());
+        for (String[] strings : array) {
+            for (int j = 0; j < array.length; j++) {
+                if (str.equals(strings[j])) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private boolean equalCharactersVertical(String checker, String stepChecker) {
@@ -61,10 +131,6 @@ public class UpdateField {
 
     private boolean equalNumbersHorizontal(String checker, String stepChecker) {
         return checker.regionMatches(3, stepChecker, 2, 1);
-    }
-
-    private FindIndexTuple2 findIndex(String[][] array, String str) {
-        return new FindIndexTuple2(array, str);
     }
 
     static class FindIndexTuple2 {
